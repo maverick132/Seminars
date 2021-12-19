b15d21c3e85b0bce515563d1694dc679867a7162
Alexandr <i_am_jeweller@vk.com>
2021-12-19 10:48:30 +0300

    Tasks 001 square numbers

--- Tasks/Tasks001/Program.cs
+++ Tasks/Tasks001/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = Int32.Parse(Console.ReadLine());
+Console.WriteLine(number * number);
+
