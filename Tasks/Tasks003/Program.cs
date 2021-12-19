8f354ce45ded1f7461e9fe306805dfa2d2fbfd70
Alexandr <i_am_jeweller@vk.com>
2021-12-19 11:36:35 +0300

    Tasks 002 Comparing numbers

---
+++ Tasks/Tasks003/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число: ");
+int numberFirst = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberSecond = Int32.Parse(Console.ReadLine());
+
+if (numberFirst > numberSecond)
+{
+    Console.Write("Наибольшее число:  " + numberFirst + ". Наименьшее число:  " + numberSecond + ".");
+}
+
+else
+{
+    if (numberFirst < numberSecond)
+    {
+        Console.Write("Наибольшее число:  " + numberFirst + ". Наименьшее число:  " + numberSecond + ".");
+
+    }
+    else
+    {
+        Console.Write("Числа равны");
+    }
+}
+
