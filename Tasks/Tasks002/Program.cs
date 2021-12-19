d624a346154d1ded1198b90b97c1d2695e7b2691
Alexandr <i_am_jeweller@vk.com>
2021-12-19 11:04:08 +0300

    Tasks 002 First is square Second numbers?

---
+++ Tasks/Tasks002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int numberFirst = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberSecond = Int32.Parse(Console.ReadLine());
+
+if (numberSecond * numberSecond  == numberFirst)
+{
+    Console.Write("Число " + numberFirst + " является квадратом числа " + numberSecond);
+}
+
+else
+{
+    Console.Write("Число " + numberFirst + " не является квадратом числа " + numberSecond);
+}
