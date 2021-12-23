bc327055ae842e092ae18df6944d92c2beae6197
Alexandr <maverick132@rambler.ru>
2021-12-23 14:21:44 +0300

    Task 010 decided

---
+++ Tasks/Task010_Second_digit_of_number/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите число от 100 до 999 ");
+int number= int.Parse(Console.ReadLine());
+
+
+Console.Write(number.ToString()[1]);
+
