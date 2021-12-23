8a20310ccb32add73459cba19b77b07c20a0f270
Alexandr <maverick132@rambler.ru>
2021-12-23 14:06:29 +0300

    Task 007 decided

---
+++ Tasks/Task007_Show_numbers_from_N_to_N/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать числа от -N до N
+
+Console.Write("Введите число: ");
+int number = Int32.Parse(Console.ReadLine());
+
+for (int i = -1 * number; i < number + 1; i++)
+{
+    Console.Write(i + " ");
+}
