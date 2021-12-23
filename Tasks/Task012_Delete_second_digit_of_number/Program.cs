c8cd17d73bbe0372cb2bb53413097652f1302e93
Alexandr <maverick132@rambler.ru>
2021-12-23 14:53:32 +0300

    Task012 decided

--- Tasks/Task012_Delete_second_digit_of_number/Program.cs
+++ Tasks/Task012_Delete_second_digit_of_number/Program.cs
@@ -1,3 +1,7 @@
 ﻿// Удалить вторую цифру трёхзначного числа
 
+Console.Write("Введите число от 100 до 999 ");
+string number = Console.ReadLine();
+number = number.Remove(1,1);
+Console.Write(number);
 
