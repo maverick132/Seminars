e3d224ce3759d64dacf4085ebff70498121e8b21
Alexandr <maverick132@rambler.ru>
2021-12-23 15:12:38 +0300

    Task 014 decided

---
+++ Tasks/Task014_Third_digit_of_number/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введите число: ");
+
+string numberString = Console.ReadLine();
+
+if (numberString.Contains("-"))
+{
+    numberString = numberString.Remove(0, 1);
+}
+
+if (numberString.Length > 2)
+{
+    int number = int.Parse(numberString);
+    Console.Write("Третья цифра данного числа: " + int.Parse(numberString).ToString()[2]);
+}
+else
+{
+    Console.Write("В данном числе нету третьей цифры");
+}
+
