638a4bce57ba2e1a06e3ecd54cd46adfa9189016
Alexandr <maverick132@rambler.ru>
2021-12-23 14:09:43 +0300

    Task 008 decided

---
+++ Tasks/Task008_Show_ODD_from_1_to_N/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Введите число: ");
+int number = Int32.Parse(Console.ReadLine());
+
+for (int i = 1; i < number + 1; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
