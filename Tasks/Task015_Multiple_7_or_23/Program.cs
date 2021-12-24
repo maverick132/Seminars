0f84bac27b7b5086c0eca7b1469909949f061dac
Alexandr <maverick132@rambler.ru>
2021-12-24 15:17:24 +0300

    Task 015 decided

---
+++ Tasks/Task015_Multiple_7_or_23/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 7 == 0 || number % 23 == 0)
+{
+    if (number % 7 == 0 && number % 23 != 0)
+    {
+        Console.WriteLine("Число кратно 7");
+        
+    }
+
+    if (number % 7 != 0 && number % 23 == 0)
+    {
+        Console.WriteLine("Число кратно 23");
+        
+    }
+
+    if (number % 7 == 0 && number % 23 == 0)
+    {
+        Console.WriteLine("Число кратно 7 и 23");
+        
+    }
+}
+else {
+     Console.WriteLine("Число некратно ни 7, ни 23");
+}
