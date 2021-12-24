001cde1043b215f5913986ebcb2b7bd30729a001
Alexandr <maverick132@rambler.ru>
2021-12-24 15:31:25 +0300

    Task 017 decided

---
+++ Tasks/Task017_It_is_Square_First_Of_Second/Program.cs
@@ -0,0 +1,24 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Write("Введите первое число: ");
+int numberFirst = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberSecond = Int32.Parse(Console.ReadLine());
+
+if (numberSecond * numberSecond == numberFirst)
+{
+    Console.Write("Число " + numberFirst + " является квадратом числа " + numberSecond);
+}
+
+else
+{
+    if (numberFirst * numberFirst == numberSecond)
+    {
+        Console.Write("Число " + numberSecond + " является квадратом числа " + numberFirst);
+    }
+    else
+    {
+        Console.Write("Число " + numberFirst + " не является квадратом числа " + numberSecond + ".");
+        Console.Write(" Число " + numberSecond + " не является квадратом числа " + numberFirst + ".");
+    }
+}
