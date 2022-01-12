cc58d24381e6412b05b35419eecffddefd19f05b
Alexandr <i_am_jeweller@vk.com>
2022-01-12 15:38:04 +0300

    Task 020 decided

---
+++ Tasks/Task020_Number_of_quadrant/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Введите номер четверти");
+
+switch (Convert.ToInt32(Console.ReadLine())){
+    case 1 : 
+    Console.WriteLine("Диапазон возможных координат по х от 0 до 100, по у от 0 до 100");
+    break;
+    case 2 : 
+    Console.WriteLine("Диапазон возможных координат по х от 0 до -100, по у от 0 до 100");
+    break;
+    case 3 : 
+    Console.WriteLine("Диапазон возможных координат по х от 0 до -100, по у от 0 до -100");
+    break;
+    case 4 : 
+    Console.WriteLine("Диапазон возможных координат по х от 0 до 100, по у от 0 до -100");
+    break;
+    default:
+    Console.WriteLine("Неверно введен номер квадранта");
+    break;
+}
+
