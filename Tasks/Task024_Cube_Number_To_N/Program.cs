262edfe3744c0e78d10888e29c15206c1ddde1eb
Alexandr <i_am_jeweller@vk.com>
2022-01-13 00:43:38 +0300

    Task024 decided

---
+++ Tasks/Task024_Cube_Number_To_N/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= N; i++)
+{
+    Console.WriteLine($"Куб числа {i} это {degree(i, 3)}");
+}
+
+Double degree(int number, int deg)
+{
+    if (deg == 1) return number;
+    return (double)number * degree(number, deg -1);
+}
