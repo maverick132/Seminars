21f224c1d55bc65e68889fa75d3730eb80e7422c
Alexandr <i_am_jeweller@vk.com>
2022-01-12 16:30:34 +0300

    Task 021 decided

---
+++ Tasks/Task021_Palindrom/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+Console.WriteLine("Введите число или слово для проверки на палиндром");
+string data = Console.ReadLine();
+
+if (CheckPalindrom(data)) {
+    Console.WriteLine($"Введенное число или слово {data} является палиндромом");
+}
+else {
+    Console.WriteLine($"Введенное число или слово {data} не является палиндромом");
+}
+
+bool CheckPalindrom(string palindrom)
+{
+    
+    for (int i = 0; i <= palindrom.Length / 2; i++)
+    {
+        if (palindrom[i] != palindrom[palindrom.Length - 1 - i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
