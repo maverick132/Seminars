0a83272eafde68c9b9dffbe2b0691471a2c64748
Alexandr <i_am_jeweller@vk.com>
2022-01-15 19:48:41 +0300

    Task 027 decided

---
+++ Tasks/Task027_Quantity_Digit_in_Number/Program.cs
@@ -0,0 +1,4 @@
+﻿// Определить количество цифр в числе
+Console.WriteLine("Введите число:");
+String number = Console.ReadLine() ?? String.Empty;
+Console.WriteLine($"Количество цифр в числе {number} равно {number.Length}");
