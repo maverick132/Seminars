1993babf11d4dd1ce3bdba4c914db4ab792ef225
Alexandr <i_am_jeweller@vk.com>
2022-01-15 19:40:03 +0300

    Task 025 asnd 026 decided

---
+++ Tasks/Task025_Sum/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму чисел от 1 до А
+int SumNumber(int finalNumber) //Использование рекурсии в качестве обучения. Но более рационально воспользоваться формулой суммы N членов арифмитической прогрессии
+{
+    if (finalNumber == 1) return 1;
+    else return SumNumber(finalNumber - 1) + finalNumber;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(SumNumber(number));
+
+
+
+
+
+
+
---
+++ Tasks/Task026_Degree_A_to_B/Program.cs
@@ -0,0 +1,22 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Double AscensionToDegree(int basis, int degree) // Однако тип Double не точен, при больших вычислениях
+{
+    Double result = (double)basis;
+    
+    for (int i = 1; i < Math.Abs(degree); i++)
+    {
+                result *= basis;
+    }
+    return degree >=0 ? result : 1/result;
+    
+
+}
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine()  ?? String.Empty);
+Console.WriteLine("Введите степень:");
+int degree = Convert.ToInt32(Console.ReadLine()  ?? String.Empty);
+
+System.Console.WriteLine($"Число {number} в степени {degree} равно {AscensionToDegree(number, degree)}");
+
