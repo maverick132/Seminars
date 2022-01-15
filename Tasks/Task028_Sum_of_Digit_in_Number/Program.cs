be5918b4cfb40f17d989bea41e6bb5631a175e5d
Alexandr <i_am_jeweller@vk.com>
2022-01-15 20:34:14 +0300

    Task 028 debug

---
+++ Tasks/Task028_Sum_of_Digit_in_Number/Program.cs
@@ -0,0 +1,15 @@
+﻿// Подсчитать сумму цифр в числе
+int SumDigit(int number)
+{
+    if (number < 10) return number;
+    else {
+        int lastDigit = number % 2;
+        number /= 10;
+        return lastDigit + SumDigit(number);
+    }
+}
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine() ?? String.Empty);
+Console.WriteLine($"Сумма всех цифр числа {number} равна {SumDigit(number)}");
