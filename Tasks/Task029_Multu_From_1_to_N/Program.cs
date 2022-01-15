c791aa8432045f55a1be91f94317870b4d59e851
Alexandr <i_am_jeweller@vk.com>
2022-01-15 22:04:34 +0300

    Task 029 decided

---
+++ Tasks/Task029_Multu_From_1_to_N/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+long Multy(int number) {
+    if (number == 1) return 1;
+    return number*Multy(number-1);
+}
+
+Console.WriteLine("Введите число");
+
+int numb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {numb} равно {Multy(numb)}");
