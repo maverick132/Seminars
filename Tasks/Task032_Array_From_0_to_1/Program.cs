c2c0dc87992dd0546a242017101b4beaa9f3c113
Alexandr <i_am_jeweller@vk.com>
2022-01-16 12:24:35 +0300

    Task 032 decided

---
+++ Tasks/Task032_Array_From_0_to_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+
+void FillArray(int[] array, int minNumber = 0, int maxNumber = 9)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minNumber, maxNumber + 1);
+    }
+}
+
+
+
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}.");
+    }
+    Console.WriteLine();
+}
+
+int[] array = new int[8];
+FillArray(array, 0, 1);
+PrintArray(array);
