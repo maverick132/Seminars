01c7c9955a3753918a6beff55d2d27880fc29143
Alexandr <i_am_jeweller@vk.com>
2022-01-22 14:17:11 +0300

    Task 041 decoded

---
+++ Tasks/Task041_Side_Triangle/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить являются ли три числа сторонами треугольника 
+
+Console.WriteLine("Введите первое число");
+int sideAB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int sideBC = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int sideCA = Convert.ToInt32(Console.ReadLine());
+
+if (sideAB+sideBC > sideCA && sideCA+sideBC > sideAB  && sideAB+sideCA > sideBC) Console.WriteLine("Числа являются стронами треугольника");
+else Console.WriteLine("Числа не являются стронами треугольника");
