55303f009a567fb9973d282ceaa9ebec0c41d460
Alexandr <i_am_jeweller@vk.com>
2022-01-22 13:54:33 +0300

    Task 037 and 038 decided

---
+++ Tasks/Task037_Count_Numbers_Section/Program.cs
@@ -0,0 +1,38 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+void FillArray(int[] array, int minNumber = 0, int maxNumber = 9)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minNumber, maxNumber + 1);
+    }
+}
+
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}.");
+    }
+    Console.WriteLine();
+}
+
+int CountArrayOnSection(int[] array, int startSection = 10, int endSection = 99)
+{
+    int countNumber = 0;
+    
+    foreach (int nextArrayNumber in array)
+    {
+        if (nextArrayNumber >= startSection && nextArrayNumber <= endSection) countNumber++;
+    }
+    return (countNumber);
+}
+
+int[] array = new int[123];
+FillArray(array, 1, 999);
+PrintArray(array);
+int numberInSetction= CountArrayOnSection(array);
+System.Console.WriteLine($"Количество чисел на отрезке [10, 99] равно {numberInSetction}");
+
---
+++ Tasks/Task038_Summ_Odd_Position_Array/Program.cs
@@ -0,0 +1,39 @@
+﻿//  Найти сумму чисел одномерного массива стоящих на нечетной позиции
+void FillArray(int[] array, int minNumber = 0, int maxNumber = 9)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minNumber, maxNumber + 1);
+    }
+}
+
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}.");
+    }
+    Console.WriteLine();
+}
+
+int SumNumberInArrayOddPosition(int[] array)
+{
+    int sum = 0;
+
+    for (int i = 1; i < array.Length; i+= 2)
+    {
+
+     sum+= array[i];  
+           
+    }
+    return (sum);
+}
+
+int[] array = new int[6];
+FillArray(array, 1, 999);
+PrintArray(array);
+int sum = SumNumberInArrayOddPosition(array);
+System.Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях равна {sum}");
