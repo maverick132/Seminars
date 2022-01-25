b53a6787fd5df6f42bb9dd7783306bce104de031
Alexandr <maverick132@rambler.ru>
2022-01-25 14:47:55 +0300

    Task 047 decided

---
+++ Tasks/Task047_Array_Copy/Program.cs
@@ -0,0 +1,38 @@
+﻿// Написать программу копирования массива
+
+void FillArray(int[] array, int minNumber = 0, int maxNumber = 9)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(minNumber, maxNumber + 1);
+    }
+}
+
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}.");
+    }
+    Console.WriteLine();
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        newArray[i] = array[i];
+    }
+    return newArray;
+}
+
+int[] array = new int[6];
+int[] arraySecond = new int[6];
+FillArray(array);
+PrintArray(array);
+arraySecond = CopyArray(array);
+PrintArray(arraySecond);
