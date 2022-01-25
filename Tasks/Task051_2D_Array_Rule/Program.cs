1fef2eb51ede4f373b2f4293dbaff7d099e029d2
Alexandr <maverick132@rambler.ru>
2022-01-25 15:58:02 +0300

    Task 051 decided

---
+++ Tasks/Task051_2D_Array_Rule/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void FillArrayRule(int[,] array, int minNumber = 0, int maxNumber = 9) //Rule -A[m,n] = m+n
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == array.GetLength(1) - 1) Console.Write($"{array[i, j]}.");
+            else Console.Write($"{array[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+FillArrayRule(array);
+PrintArray(array);
+
