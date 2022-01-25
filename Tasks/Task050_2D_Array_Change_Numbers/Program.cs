f62f3b3c76cc552b042b59ea73891e01395f5ba0
Alexandr <maverick132@rambler.ru>
2022-01-25 15:54:24 +0300

    Task 050 decided

---
+++ Tasks/Task050_2D_Array_Change_Numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+
+void FillArray(int[,] array, int minNumber = 0, int maxNumber = 9)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minNumber, maxNumber + 1);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == array.GetLength(1) - 1) Console.Write($"{array[i, j]}.");
+            else Console.Write($"{array[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ReplaceOnOppositeEven(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = 0 - array[i, j];
+        }
+    }
+}
+
+
+Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+ReplaceOnOppositeEven(array);
+PrintArray(array);
