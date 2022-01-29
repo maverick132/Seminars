b56dfb89b22cf9c715c2a4d980ac3b275a150d88
Alexandr <i_am_jeweller@vk.com>
2022-01-29 22:51:46 +0300

    Task 072 decided

---
+++ Tasks/Task072_A_Pow_B/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+double NumberPow(int number, int pow)
+{
+    if (pow == 0) return 1;
+    if (pow == 1) return (double)number;
+    if (pow > 0) return (double)number * NumberPow(number, pow - 1);
+    return 1 / (double)number * NumberPow(number, pow + 1);
+}
+
+int number = 2;
+int pow = -4;
+Console.WriteLine(Math.Round(NumberPow(number, pow), 3));
