f8fc0bd2a15c93500652113a92df6df88a9b98a6
Alexandr <i_am_jeweller@vk.com>
2022-01-29 19:56:24 +0300

    Task 070 decided

---
+++ Tasks/Task070_Sum_Number_Digit/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти сумму цифр числа
+
+int SumDigitNumber(int number)
+{
+    if (number / 10 == 0) return number;
+    return number % 10 + SumDigitNumber(number / 10);
+}
+
+Console.WriteLine(SumDigitNumber(234));
