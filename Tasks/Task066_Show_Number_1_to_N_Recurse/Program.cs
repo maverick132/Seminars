ab25ba6b93d11cf7448239a21cec392dce883b8d
Alexandr <i_am_jeweller@vk.com>
2022-01-29 11:51:36 +0300

    Task 066 and 067 decided

---
+++ Tasks/Task066_Show_Number_1_to_N_Recurse/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+void ShowNumberToN(int number)
+{
+    if (number == 1) Console.WriteLine(number);
+    else
+    {
+        ShowNumberToN(number - 1);
+        Console.WriteLine(number);
+    }
+}
+
+int number = 10;
+ShowNumberToN(number);
+
---
+++ Tasks/Task067_Show_Number_N_To_1_Recurse/Program.cs
@@ -0,0 +1,14 @@
+﻿// // Показать натуральные числа от N до 1, N задано
+
+void ShowNumberToN(int number)
+{
+    if (number == 1) Console.WriteLine(number);
+    else
+    {
+        Console.WriteLine(number);
+        ShowNumberToN(number - 1);
+    }
+}
+
+int number = 10;
+ShowNumberToN(number);
