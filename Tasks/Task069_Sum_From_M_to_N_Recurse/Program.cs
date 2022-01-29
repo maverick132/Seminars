cd3482db5ac23b04aa9574864e245bb7d249c50d
Alexandr <i_am_jeweller@vk.com>
2022-01-29 16:38:33 +0300

    Task 069 decided

---
+++ Tasks/Task069_Sum_From_M_to_N_Recurse/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumNumber(int firstNumber, int secondNumber)
+{
+    if (firstNumber == secondNumber) return firstNumber;
+    else if (firstNumber > secondNumber) return firstNumber + SumNumber(firstNumber - 1, secondNumber);
+        else return firstNumber + SumNumber(firstNumber + 1, secondNumber);
+
+}
+
+int sum = SumNumber(11, 1);
+
+Console.WriteLine(sum);
+
