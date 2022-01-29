dc29a0a63af9cdf68f16c2a7ea84661d044fd769
Alexandr <i_am_jeweller@vk.com>
2022-01-29 22:45:51 +0300

    Task 071 decided

---
+++ Tasks/Task071_Akkerman_Function/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int AckermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return AckermanFunction(m - 1, 1);
+    return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+}
+
+int m = 4;
+int n = 0;
+Console.WriteLine(AckermanFunction(m, n));
