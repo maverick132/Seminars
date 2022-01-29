caa731bcf815af908cb88c915d87b64c6aba3fc4
Alexandr <i_am_jeweller@vk.com>
2022-01-29 16:33:49 +0300

    Task 068 decided

---
+++ Tasks/Task068_Show_Number_M_to_N/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void ShowNumberToN(int firstNumber, int secondNumber)
+{
+    if (firstNumber == secondNumber) Console.WriteLine(firstNumber);
+    else
+    {
+        if (firstNumber > secondNumber)
+        {
+            Console.WriteLine(firstNumber);
+            ShowNumberToN(firstNumber - 1, secondNumber);
+        }
+        else
+        {
+             ShowNumberToN(firstNumber, secondNumber - 1);
+            Console.WriteLine(secondNumber);
+        }
+    }
+}
+
+int number = 10;
+ShowNumberToN(10, 1);
+ShowNumberToN(1, 10);
